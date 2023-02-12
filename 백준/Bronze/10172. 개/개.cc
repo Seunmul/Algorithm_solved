68ea76cf25be8c52e373a7c6544ffa4a8c8780c7
Seunmul <87626380+Seunmul@users.noreply.github.com>
2023-02-12 22:32:27 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+
+*/
+
+int main()
+{
+    cout << "|\\_/|" << endl;
+    cout << "|q p|   /}" << endl;
+    cout << "( 0 )\"\"\"\\" << endl;
+    cout << "|\"^\"`    |" << endl;
+    cout << "||_/=\\\\__|" << endl;
+    // cout <<  << endl;
+    // cout <<  << endl;
+
+    return 0;
+}
