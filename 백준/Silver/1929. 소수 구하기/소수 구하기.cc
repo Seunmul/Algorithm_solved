2adf14531d34a8f3f28ead1ccc34938e073d4071
Seunmul <87626380+Seunmul@users.noreply.github.com>
2023-02-16 02:51:20 +0900

    [Silver III] Title: 소수 구하기, Time: 100 ms, Memory: 1128 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -1,18 +1,14 @@
-#include <bits/stdc++.h>
-using namespace std;
-bool cal(int n){
-    if(n==1 or (n%2==0 and n!=2)) return false;
-    double tmp = sqrt(n);
-    for(int i=3;i<abs(tmp)+1;i+=2)
-        if(n%i==0) return false;
-    return true;
-}
+#include <stdio.h> 
+#include <math.h>
 int main(){
-    int M,N;
-    cin >> M >> N;
-    for(int i=M;i<=N;i++)
-        if(cal(i))
-            cout<<i<<"\n";
-
+    int N,M,c=1; 
+    scanf("%d %d",&N,&M);
+    for(int i=N;i<=M;i++){
+        if(i==1 or (i%2==0 and i!=2)) continue;
+        int tmp = sqrt(i)+1;
+        for(int k=3;k<tmp;k+=2) if(i%k==0) {c=0; break;}
+        if(c==1) printf("%d\n",i);
+        c=1;
+    } 
     return 0;
 }
