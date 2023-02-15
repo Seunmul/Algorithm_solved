8617cbbef3851b07c0a51d65c73254790f88c95a
Seunmul <87626380+Seunmul@users.noreply.github.com>
2023-02-16 01:59:57 +0900

    [Silver V] Title: 소수 찾기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.cc
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.cc
@@ -3,19 +3,11 @@
 using namespace std;
 
 bool cal(int n){
-    // cout << "입력:"<<n<<"\n";
-    if(n<0 or n>1001) return false;
-    if(n==1) return false;
-    else if(n==2) return true;
-    else if(n%2==0) return false;
-
+    if(n==1 or (n%2==0 and n!=2)) return false;
     double tmp = sqrt(n);
-	if (abs(tmp - (int)tmp < 1e-12)) return false;
-	else
-        for(int i=3;i<abs(tmp)+1;i+=2)
-            if(n%i==0) return false;
+    for(int i=3;i<abs(tmp)+1;i+=2)
+        if(n%i==0) return false;
     return true;
-    
 }
 
 int main(){
@@ -24,13 +16,9 @@ int main(){
     cin >> N;
     for(int i=0;i<N;i++)
         cin>>arr[i];
-    
-
-
     for(int i=0;i<N;i++)
         if(cal(arr[i]))
             cnt++;
-    
     cout << cnt;
     return 0;
 }
