fa8298496981fbf1cae478e3fe10bc23fe3b1d9e
Seunmul <87626380+Seunmul@users.noreply.github.com>
2023-02-16 00:23:38 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+    int x,y,w,h;
+    cin >> x >> y >> w >> h;
+    int arr[4]={x,y,w-x,h-y};
+    sort(arr,arr+4);
+    cout<<arr[0]<<"\n";    
+    return 0;
+}
