fab397cfb2f0cee37f84fe26ac54d34286bb59d1
Seunmul <87626380+Seunmul@users.noreply.github.com>
2023-02-16 02:10:15 +0900

    [Silver V] Title: 소수 찾기, Time: 0 ms, Memory: 1128 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.cc
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.cc
@@ -1,20 +1,14 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <math.h>
-
 bool cal(int n){
     if(n==1 or (n%2==0 and n!=2)) return false;
-    double tmp = sqrt(n);
-    for(int i=3;i<abs(tmp)+1;i+=2)
-        if(n%i==0) return false;
+    double tmp = sqrt(n); for(int i=3;i<abs(tmp)+1;i+=2) if(n%i==0) return false;
     return true;
 }
-
 int main(){
-    int N,cnt=0;
-    scanf("%d",&N);
-    int v[N];
+    int N,cnt=0; scanf("%d",&N); int v[N];
     for(int i=0;i<N;i++) scanf("%d",v+i);
-    for(int i:v) if(cal(i)) cnt++;
+    for(int i:v) if(cal(i)) cnt++; 
     printf("%d",cnt);
     return 0;
 }
