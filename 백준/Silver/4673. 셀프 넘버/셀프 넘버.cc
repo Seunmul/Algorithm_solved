85a7df2c696b62bda29a114c1e2c723c3c2306f8
Seunmul <87626380+Seunmul@users.noreply.github.com>
2023-02-18 23:17:12 +0900

    [Silver V] Title: 셀프 넘버, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.cc
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.cc
@@ -1,16 +1,13 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
-void chk(int res,int arr[]){
-    string tmp=to_string(res);
-    for(unsigned long i=0;i<tmp.size();i++) res += tmp[i]-48;
-    if(res<=10000) arr[res]=1;
-}
-
 int main(){
     int arr[10001]={0,};
-    for(int i=1;i<=10000;i++) chk(i,arr);
+    for(int i=1;i<=10000;i++) {
+        string tmp=to_string(i);
+        for(char c:tmp) i += c-48;
+        if(i<=10000) arr[i]=1;
+        i=atoi(tmp.c_str());
+    }
     for(int i=1;i<=10000;i++) if(!arr[i]) cout<<i<<"\n";
     return 0;
 }
