d65dc24d47878338574af1ee93c23057e8a6aa26
Seunmul <87626380+Seunmul@users.noreply.github.com>
2023-02-19 19:05:36 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 220 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+int T, A, B;
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> T;
+    while (T--){
+        cin >> A >> B; cout << A + B<<"\n"; 
+    }
+    return 0;
+}
