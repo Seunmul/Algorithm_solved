e007c44279241e65a4c39fd5e063218cfebc0e39
Seunmul <87626380+Seunmul@users.noreply.github.com>
2023-02-20 17:22:30 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int A,B;
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+    while(cin >> A >> B and (A!=0 and B!=0))
+        cout<<A+B<<"\n";
+    return 0;
+}
