f22b202cc6b1ac12f73c21e59718461804248f8f
Seunmul <87626380+Seunmul@users.noreply.github.com>
2023-02-20 22:56:32 +0900

    [Silver IV] Title: 한수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.cc
+++ 백준/Silver/1065. 한수/한수.cc
@@ -2,22 +2,16 @@
 using namespace std;
 bool chk(string N){
     vector<int> v;
-    if(N.length()<=2) return true;
-    for(unsigned int i=0;i<N.length();i++)
-        v.push_back(N[i]-48);
-    for(unsigned int i=1;i<v.size()-1;i++)
-        // cout << "사이즈:"<< v.size() << "숫자 :" << N<< "\n";
+    for(char c:N) v.push_back(c-48);
+    for(unsigned int i=1;i<N.length()-1;i++)
         if((v.at(i)-v.at(i+1))!=(v.at(i-1)-v.at(i)))
             return false;
     return true;
 }
 int main(){
-    int N;
+    int N,tmp=0,cnt=0;
     cin >> N;
-    int tmp=1,cnt=0;
-    while(tmp<=N)
-        if(chk(to_string(tmp++)))
-            cnt++;
+    while(++tmp<=N) if((tmp<100) or chk(to_string(tmp))) cnt++;
     cout << cnt;
     return 0;
 }
