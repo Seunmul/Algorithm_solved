a18ffe04088406f5bd2cf62ab52ee19ef9f5c4c2
Seunmul <87626380+Seunmul@users.noreply.github.com>
2023-02-23 00:08:10 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 672 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int T;
+int main(){
+    cin >> T;
+    vector<int> v(T);
+    for(int i=0;i<T;i++) cin >> v.at(i);
+    sort(v.begin(),v.end());
+    for(int i:v) cout << i << "\n";
+    return 0;
+}
