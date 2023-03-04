0dcddfc41c701623f06feabb302874b710064612
Seunmul <87626380+Seunmul@users.noreply.github.com>
2023-03-04 23:58:44 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -2,30 +2,18 @@
 using namespace std;
 int main()
 {
-    int arr[9] = {
-        0,
-    };
+    int arr[9] = {0,};
+    for (int i = 0; i < 9; i++) cin >> arr[i];
+    sort(arr, arr + 9);
     for (int i = 0; i < 9; i++)
-        cin >> arr[i];
-    sort(arr,arr+9);
-    for (int i = 0; i < 9; i++)
-    {
         for (int j = i + 1; j < 9; j++)
-        {
             for (int k = j + 1; k < 9; k++)
-            {
                 for (int m = k + 1; m < 9; m++)
-                {
                     for (int n = m + 1; n < 9; n++)
-                    {
                         for (int l = n + 1; l < 9; l++)
-                        {
                             for (int p = l + 1; p < 9; p++)
-                            {
-                                int tmp = arr[i] + arr[j] + arr[k] + arr[m] + arr[n] + arr[l] + arr[p];
-                                // cout << i << " " << j << " " << k << " " << m << " " << n << " " << l << " " << p << " ";
-                                // cout << tmp << "\n";
-                                if (tmp == 100){
+                                if (arr[i] + arr[j] + arr[k] + arr[m] + arr[n] + arr[l] + arr[p] == 100)
+                                {
                                     cout << arr[i] << "\n"
                                          << arr[j] << "\n"
                                          << arr[k] << "\n"
@@ -35,13 +23,6 @@ int main()
                                          << arr[p] << "\n";
                                     return 0;
                                 }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
 
     return 0;
 }
