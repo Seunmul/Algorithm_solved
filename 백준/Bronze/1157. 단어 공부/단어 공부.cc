ec72be32d238c2804d6dd61edcfdbeb074dcb465
Seunmul <87626380+Seunmul@users.noreply.github.com>
2023-03-12 02:53:32 +0900

    [Bronze I] Title: 단어 공부, Time: 44 ms, Memory: 4916 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.cc
+++ 백준/Bronze/1157. 단어 공부/단어 공부.cc
@@ -1,37 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 string s;
-int arr[27], tmp = -1, idx = -1, cnt = 0;
+int arr[27], mx = -1, idx = -1, cnt = 0;
 int main()
 {
     cin >> s;
     for (char c : s)
-    {
-        if ((int)c >= 97 && (int)c <= 122)
-            arr[c - 'a' + 1]++;
-        else
-            arr[c - 'A' + 1]++;
-    }
-
+        if (c >= 'a' && c <= 'z') arr[c - 'a' + 1]++;
+        else arr[c - 'A' + 1]++;
     for (int i = 1; i <= 26; i++)
-    {
-        if (arr[i] > tmp)
+        if (arr[i] > mx)
         {
-            idx = i;
-            tmp = arr[i];
-            cnt = 0;
+            idx = i; mx = arr[i]; cnt = 0;
         }
-        else if (arr[i] == tmp)
-        {
-            cnt++;
-        }
-    }
-
-    if (cnt > 0)
-        cout << "?"
-             << "\n";
-    else
-        cout << (char)(idx + 'A' - 1) << "\n";
-
+        else if (arr[i] == mx) cnt++;
+    if (cnt > 0) cout << "?" << "\n";
+    else cout << (char)(idx + 'A' - 1) << "\n";
     return 0;
 }
